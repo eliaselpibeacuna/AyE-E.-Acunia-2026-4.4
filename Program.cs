b10ef5805c9440145cc292e71d054364f2faf8ab
Eliaselcrack777acuna <eliasacunaet32@gmail.com>
2026-03-19 11:21:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,248 @@
+﻿Console.WriteLine("25 > 30");
+
+if (25 > 30)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+}
+
+Console.WriteLine("14 <= 14");
+
+if (14 <= 14)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+
+Console.WriteLine("0==0");
+
+if (0==0)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("10 != 10");
+
+if (10 != 10)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+
+Console.WriteLine("8 >= 3");
+
+if (8 >= 3)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("15 < 15");
+
+if (15 < 15)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("100 != 50");
+
+if (100 != 50)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("7 == 7.0");
+
+if (7 == 7.0)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("0.5 > 0");
+
+if (0.5 > 0)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("-5 < -2");
+
+if (-5 < -2)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("(5 > 3) AND (10 < 20)");
+
+if ((5 > 3) && (10 < 20))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("(8 == 10) OR (5 != 2)");
+
+if ((8 == 10) || (5 != 2))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("NOT (10 == 10)");
+
+if (!(10 == 10))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("(15 > 10) AND NOT (5 > 10)");
+
+if ((15 > 10) && !(5 > 10))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("(10 + 2 > 10) OR (5 * 2 < 8)");
+
+if ((10 + 2 > 10) || (5 * 2 < 8))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("Si temperatura = 35 y Humedad = Falso: (temperatura > 30) AND (humedad == Verdadero)");
+
+int temperatura = 35;
+Boolean Humedad = false;
+if (temperatura > 30 && Humedad == true)
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("Si edad = 16: NOT (edad >= 18)");
+
+int edad = 16;
+
+if (!(edad >= 18))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("(10 / 3 == 3) AND (10 % 3 == 1)");
+
+if ((10 / 3 == 3) && (10 % 3 == 1))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("Si nombre = \"Ana\": (nombre == \"Juan\") OR (nombre == \"Ana\")");
+
+string nombre = ("Ana");
+
+if (nombre == "Juan" || nombre == "Ana")
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
+Console.WriteLine("((5 * 2) + 10 > 15) AND (20 / 4 == 5)");
+
+if (((5 * 2) + 10 > 15) && (20 / 4 == 5))
+{
+    Console.WriteLine("La afirmacion es verdadera");
+
+}
+else
+{
+    Console.WriteLine("La afirmacion es falsa");
+
+}
