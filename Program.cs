5f511b989f44c5dfbf23ee87e42f12e46b265676
Eliaselcrack777acuna <eliasacunaet32@gmail.com>
2026-04-07 13:25:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Ejercicio 1: Contraseña secreta
+
+Console.WriteLine("Ingrese la contraseña secreta (en definitiva no es Quesito777)");
+String Contraseña = Console.ReadLine();
+while (Contraseña != "Quesito777"){
+
+    Console.WriteLine("Contraseña incorrecta, intentelo de nuevo");
+    Contraseña = Console.ReadLine();
+}
+Console.WriteLine("Contraseña correcta, Entrando...");
+
+// Ejercicio 2: Despegue!
+
+Console.WriteLine("");
+Console.WriteLine("A Continuacion una cuenta regresiva antes del despegue");
+for (int x = 5; x >= 1; x--)
+{
+    Console.WriteLine(x);
+}
+Console.WriteLine("¿Listo para despegar?");
+Console.WriteLine("¡¡Despegue!!");
+
+// Ejercicio 3: Numero Secreto
+
+Console.WriteLine("");
+Console.WriteLine("Adivina el numero secreto");
+int numsecreto = Convert.ToInt32(Console.ReadLine());
+while (numsecreto != 7)
+{
+    Console.WriteLine("Numero erroneo");
+    numsecreto = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Adivinaste!!");
+
+// Ejercicio 4: Muchas sumas!
+Console.WriteLine("");
+Console.WriteLine("Escriba numeros para sumarlos, si quiere terminar la suma ingrese 0");
+int acum = 0;
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+while (num1 != 0) {
+
+    acum = acum + num1;
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+}
+Console.WriteLine("La suma total es de");
+Console.Write(acum);
